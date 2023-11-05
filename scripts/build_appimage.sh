cdf124e910b94282dcc31dd28350ca8ae695e337
Ruben Cano Diaz <ruben.cano96@gmail.com>
2023-11-05 20:27:34 +0100

    copy deps recursive

--- scripts/build_appimage.sh
+++ scripts/build_appimage.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+function copy_deps {
+    local dep=$1
+    local target_dir=$2
+
+    # Copy the dependency
+    cp $dep $target_dir
+
+    # Get the dependencies of the dependency
+    local deps=$(ldd $dep | cut -d' ' -f3)
+
+
+    # For each dependency of the dependency, call this function recursively
+    for dep in $deps; do
+        if [ ! -f $target_dir/$(basename $dep) ]; then
+            copy_deps $dep $target_dir
+        fi
+    done
+}
+
+
 cp -rf ../build/linux/x64/release/bundle/* FEhViewer.AppDir/
-ldd FEhViewer.AppDir/fehviewer | cut -d' ' -f3  | xargs -I {} cp {} FEhViewer.AppDir/lib
+deps=$(ldd FEhViewer.AppDir/fehviewer | cut -d' ' -f3)
+# | xargs -I {} copy_deps {} FEhViewer.AppDir/lib
+
+for dep in $deps; do
+    copy_deps $dep FEhViewer.AppDir/lib
+done
+
 unset SOURCE_DATE_EPOCH
 
 if [ ! -f appimagetool-x86_64.AppImage ]; then
